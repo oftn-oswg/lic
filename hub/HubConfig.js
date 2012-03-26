608dac55070e42b64f797d147a6be37014aa2b3e
devyn <devyn.cairns@gmail.com>
2012-03-26 15:35:47 -0700

    Add more config defaults.

--- hub/HubConfig.js
+++ hub/HubConfig.js
@@ -6,7 +6,7 @@ var FileUtils = require ("./FileUtils.js");
 
 var HubConfig = function (event_manager) {
 
-	this.path = null;
+	this.location = path.join (FileUtils.home (), ".lic", "config.json");
 
 	this.event_manager = event_manager;
 
@@ -15,11 +15,14 @@ var HubConfig = function (event_manager) {
 
 	// lic provider
 	this.data.lic            = {};
-	this.data.lic.interfaces = ["unix:/tmp/lic.sock"];
-	this.data.lic.routes     = {};
+	this.data.lic.autoload   = [];                     // List of Hub scripts (in-process petals) to autoload.
+	this.data.lic.autostart  = [];                     // List of executable scripts to run automatically upon Hub startup.
+	this.data.lic.interfaces = ["unix:/tmp/lic.sock"]; // List of network interfaces to accept connections on.
+	                                                   //   Acceptable values: unix:<path>, tcp:host:port, tcp-ssl:host:port
+	this.data.lic.password   = "";                     // If non-empty, out-of-process petals need to authenticate with the
+	                                                   //   specified password to register.
+	this.data.lic.routes     = {};                     // The lic petal routing tree as "parent":["child1",...] pairs.
 };
-
-HubConfig.prototype.location = path.join (FileUtils.home (), ".lic", "config.json");
 
 
 HubConfig.prototype.load = function (callback) {
