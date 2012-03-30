ed12aa457c63e874632550622f9924f417b074cf
Devin Samarin <eboyjr14@gmail.com>
2012-03-29 23:26:45 -0700

    Add sexy listener tree to ItemManager.

--- hub/ItemManager.js
+++ hub/ItemManager.js
@@ -7,8 +7,50 @@ var util = require ("util");
  * This is the main starting point for all of the events.
  */
 var ItemManager = module.exports = function () {
+	this.listener_tree = {};
 };
 
 ItemManager.prototype.send = function(event) {
 	util.puts (util.inspect (event, false, 2, true));
+};
+
+ItemManager.prototype.listen = function(items, listener, callback) {
+	if (Array.isArray (items)) {
+		if (typeof items[0] === "string") {
+			add_listener.call (this, items, listener);
+		} else {
+			items.forEach (function(item) {
+				add_listener.call (this, item, listener);
+			});
+		}
+	} else {
+		if (callback) {
+			callback.call (this, new Error("Invalid argument for listen"));
+		}
+	}
+
+	function add_listener(item, listener) {
+		var node = this.listener_tree;
+		var item_length = item.length;
+		var part;
+
+		for (var i = 0; i < item_length; i++) {
+			part = item[i];
+			if (typeof node[part] !== "object") {
+				node[part] = {};
+			}
+
+			if (i === item_length-1) {
+				// Append the listener
+				if (node[part].listeners) {
+					node[part].listeners.push (listener);
+				} else {
+					node[part].listeners = [listener];
+				}
+				break;
+			}
+
+			node = node[part];
+		}
+	}
 };
