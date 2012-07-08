8a310183884395196b1693c2598d2def8ba41781
Devin Samarin <eboyjr14@gmail.com>
2012-07-08 05:10:52 -0700

    Make IRC petal listen for commands with ItemManager

--- petal/irc/IRC.js
+++ petal/irc/IRC.js
@@ -8,7 +8,25 @@ var IRCManager = function (item_manager) {
 
 	Petal.call (this, item_manager);
 
-	this.connections = [];
+	this.servers = {};
+
+	item_manager.listen ([this.name, "*"], function(item, command) {
+		var server, args, method;
+
+		server = item[1];
+		args = Array.prototype.slice.call (arguments, 2);
+
+		if (server) {
+			if (self.servers.hasOwnProperty (server)) {
+				method = Connection.prototype[command];
+				if (typeof method === "function") {
+					method.apply (self.servers[server], args);
+					return true;
+				}
+			}
+		}
+		return false;
+	});
 
 	item_manager.command (["lic", "config"], "get", ["IRC"], function(error, values) {
 		if (error) {
@@ -17,10 +35,14 @@ var IRCManager = function (item_manager) {
 		}
 		self.config = values[0];
 		self.connect ();
+
+		console.log ("Retreived configuration");
 	});
 };
 
 util.inherits (IRCManager, Petal);
+
+IRCManager.prototype.name = "irc";
 
 /* This function takes the server profile from the config and extends
  * it with the defaults from the config so that when the defaults
@@ -32,32 +54,36 @@ IRCManager.prototype.create_profile = function (profile, defaults) {
 };
 
 IRCManager.prototype.connect = function () {
-	var connection, profile, servers, defaults;
+	var self, connection, profile, servers, defaults;
 
+	self = this;
 	servers  = this.config.servers;
 	defaults = this.config.default;
 
 	for (var i = 0, len = servers.length; i < len; i++) {
 
 		profile = this.create_profile (servers[i], defaults);
-		connection = new Connection (profile, this.item_manager);
 
-		///* The following section is a massive hack, used temporarily as a testing interface.
-		connection.on("001", function (message) {
-			var channel = "#oftn";
+		if (!this.servers.hasOwnProperty (profile.name)) {
+			connection = new Connection (profile);
 
-			this.raw ("JOIN " + channel);
-			process.stdin.resume();
-			process.stdin.on("data", function(data) {
-				connection.send ("PRIVMSG " + channel + " :" + String(data).trim());
+			console.log ("Connecting to IRC server `%s`", connection.name);
+			connection.on ("message", function(data) {
+				var item = [self.name, this.name];
+				if (data.params[0]) {
+					item.push (data.params[0]);
+				}
+
+				self.item_manager.command (item, data.command, data);
 			});
-		});
-		//*/
-	
-		console.log ("Connecting to IRC server \"%s\"", connection.name);
-		connection.connect ();
 
-		this.connections.push (connection);
+			connection.connect ();
+
+			this.servers[profile.name] = connection;
+
+		} else {
+			console.error ("IRC already has connection called `%s`. Skipping duplicate.", profile.name);
+		}
 	}
 };
 
@@ -65,7 +91,7 @@ IRCManager.prototype.shutdown = function(callback) {
 	var connections, self = this;
 
 	// First we clone the connections array to use as a queue
-	connections = this.connections.slice ();
+	connections = Object.getOwnPropertyNames (this.servers)
 	console.log ("Waiting for IRC servers to close connections...");
 
 	(function next () {
@@ -74,7 +100,7 @@ IRCManager.prototype.shutdown = function(callback) {
 			callback.call (self);
 			return;
 		}
-		c.quit (next);
+		self.servers[c].quit (next);
 	}) ();
 };
 
